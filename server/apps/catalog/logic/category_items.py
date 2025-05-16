2a3b7baf97045d546305bf7ac94d94dd4812c45c
AleksandrZhukovin <sashokzuk@gmail.com>
2025-05-16 18:10:31 +0300

    feat(catalog): Add draft catalog views

--- server/apps/catalog/logic/category_items.py
+++ server/apps/catalog/logic/category_items.py
@@ -1,12 +1,22 @@
 from django.views.generic import ListView
+from django.db.models import Prefetch
 
-from server.apps.catalog.models import Category
+from server.apps.catalog.models import Item, ItemImage
 
 
 __all__ = ('CategoryCatalogView',)
 
 
 class CategoryCatalogView(ListView):
-    model = Category
+    model = Item
     context_object_name = 'categories'
     template_name = 'catalog/catalog_index.html'
+
+    def get_queryset(self):
+        return (
+            Item.objects
+            .prefetch_related(
+                Prefetch('images', queryset=ItemImage.objects.only('image'))
+            )
+            .filter(category__pk=self.kwargs['category_id'])
+        )
--- server/apps/catalog/urls.py
+++ server/apps/catalog/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
-from django.views.decorators.cache import cache_page
 
 from server.apps.catalog.logic.catalog_index import *
+from server.apps.catalog.logic.category_items import *
 from server.apps.catalog.logic.configurator import *
 
 
 app_name = 'catalog'
 
 urlpatterns = [
-    path('', cache_page(60 * 15)(CatalogIndexView.as_view()), name='index'),
-    path('', ConfiguratorView.as_view(), name='configurator'),
+    path('', CatalogIndexView.as_view(), name='index'),
+    path('configurator/', ConfiguratorView.as_view(), name='configurator'),
+    path('category/<int:category_id>/catalog/', CategoryCatalogView.as_view(), name='category-catalog'),
 ]
