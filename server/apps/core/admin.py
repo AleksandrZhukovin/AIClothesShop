a24d1b5877e4e0cecb8bfdbf3c8247d35f69094f
AleksandrZhukovin <sashokzuk@gmail.com>
2025-08-01 16:17:35 +0300

    refactor(general): Remove unused files

--- server/apps/core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from server.apps.catalog.models import Category
-
-
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    list_filter = ('created_at', 'updated_at',)
