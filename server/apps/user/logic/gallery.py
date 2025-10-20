23bdabe49d31ce9fc9397b5e09cf90becdfd4c9c
AleksandrZhukovin <sashokzuk@gmail.com>
2025-10-20 14:45:53 +0300

    fix(gallery): Fix session gallery queryset extracting

--- server/apps/user/logic/gallery.py
+++ server/apps/user/logic/gallery.py
@@ -1,7 +1,8 @@
-from django.views.generic import ListView, DeleteView
+from django.views.generic import ListView, DeleteView, View
 from django.http import HttpResponse
 
 from server.apps.user.models import GalleryImage
+from server.apps.catalog.services.session_gallery import SessionGalleryManager
 
 
 __all__ = ("UserGalleryView", "SessionGalleryView", "GalleryImageDeleteView")
@@ -23,7 +24,7 @@ class SessionGalleryView(AbstractGalleryView):
     template_name = "user/partials/session_gallery.html"
 
     def get_queryset(self):
-        return self.model.get_session_gallery(self.request.session)
+        return SessionGalleryManager(self.request.session).get_gallery()
 
 
 class GalleryImageDeleteView(DeleteView):
@@ -32,4 +33,11 @@ class GalleryImageDeleteView(DeleteView):
 
     def form_valid(self, form):
         self.object.delete()
+        return HttpResponse()
+
+
+class SessionGalleryImageDeleteView(View):
+    def post(self, request, *args, **kwargs):
+        image_url = self.request.POST.get("image_url")
+        # GalleryImage.unauthenticated_user_delete(self.request.session, image_url)
         return HttpResponse()
