29cbf944d279bc396f16d3af4a35708a43bc29ba
AleksandrZhukovin <sashokzuk@gmail.com>
2025-10-20 11:35:39 +0300

    add(utils): Add file convertion util; refactor htmx trigger util

--- server/apps/core/utils.py
+++ server/apps/core/utils.py
@@ -1,7 +1,16 @@
+import base64
+
+from django.core.files.base import ContentFile
 from django.http import HttpResponse
 
 
-def hx_trigger(trigger, status=204):
+def hx_trigger(trigger: str, status: int = 204) -> HttpResponse:
     response = HttpResponse(status=status)
     response["HX-Trigger"] = trigger
     return response
+
+
+def base64_to_content_file(
+    image_base64: str, filename: str = "image.png"
+) -> ContentFile:
+    return ContentFile(base64.b64decode(image_base64), name=filename)
